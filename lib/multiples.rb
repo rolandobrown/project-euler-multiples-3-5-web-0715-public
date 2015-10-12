8f7770fe307ac8707ff12f6e5886cbb7a57b5d22
rolandobrown <rolando.brown@gmail.com>
2015-10-12 11:21:10 -0400

    Find the sum of all the multiples of 3 or 5 below 1000

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
-# Enter your procedural solution here!
+# Procedural
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...@limit).find_all {|n| n % 3 == 0 || n % 5 == 0}
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,8 @@
-# Enter your object-oriented solution here!
+# Object-oriented
+def collect_multiples(limit)
+  (1...limit).find_all {|n| n % 3 == 0 || n % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).reduce(:+)
+end
